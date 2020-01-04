a7328a7767bc067b2e1e1d85f859f5f8af8a649b
PerfectPan <3207113562@qq.com>
2020-01-04 22:08:18 +0800

    构造 && 数学

---
+++ Codeforces/1269A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define MP make_pair
+#define PB emplace_back
+using namespace std;
+typedef long long ll;
+template<typename T>
+inline T read(T&x){
+    x=0;int f=0;char ch=getchar();
+    while (ch<'0'||ch>'9') f|=(ch=='-'),ch=getchar();
+    while (ch>='0'&&ch<='9') x=x*10+ch-'0',ch=getchar();
+    return x=f?-x:x;
+}
+int n;
+int main(){
+    read(n);
+    printf("%d %d\n",10*n,9*n);
+    return 0;
+}
