897742cfe4e5d07da400de09d5a0e329457d91ac
PerfectPan <3207113562@qq.com>
2020-01-11 23:24:00 +0800

    math

---
+++ Codeforces/1285A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define MP make_pair
+#define PB emplace_back
+using namespace std;
+typedef long long ll;
+template<typename T>
+inline T read(T&x){
+    x=0;int f=0;char ch=getchar();
+    while (ch<'0'||ch>'9') f|=(ch=='-'),ch=getchar();
+    while (ch>='0'&&ch<='9') x=x*10+ch-'0',ch=getchar();
+    return x=f?-x:x;
+}
+const int N=1e5+10;
+int n;
+char s[N];
+int main(){
+    read(n);
+    scanf("%s",s+1);
+    printf("%d\n",n+1);
+    return 0;
+}
